d11221a17fa521885f5640c47c4fab15167167cd
samwanyua <samexample8@gmail.com>
2026-02-04 13:38:04 +0300

    Containers & scaffolds

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  /**
-   * List -> ordered collection of elemeents, can have duplicates
-   * List <int> numbers = [3,4,5,6,7,8];
-   * 
-   * Set - unordered collection of unique elements
-   * 
-   * Set<String> emails = {"antonie@dal.co.ke", "ter@dar.co.ke"};
-   * 
-   * Map -> Key-value pairs
-   * 
-   * Map users = {
-   *  'name': "Antique Brown",
-   *  'email': "antique.b@dal.co.ke",
-   *   'admNumber': 45656
-   * }
-   * 
-   */
   runApp(MyApp());
 }
 
@@ -26,9 +9,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.greenAccent,
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.amberAccent,
+              borderRadius: BorderRadius.circular(20),
+
+            ),
+            padding: EdgeInsets.all(90),
+            child: Text("Sauti Care"),
+            
+          ),
+        ),
+        
+      ),
     );
   }
 }
